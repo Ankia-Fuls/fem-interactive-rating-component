6f836691338d949926cca44c0fe211bfd7f25650
Ankia-Fuls <ankiafuls01@gmail.com>
2025-07-08 13:26:25 +0200

    Javascript Added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const submitButton = document.getElementById("submit");
+
+let chosenValue = 0;
+
 const selectButton = (id) => {
-    alert(id);
+    chosenValue = id.slice(-1);
+    document.getElementById(`${id}`).focus();
 }
+
+submitButton.addEventListener("click", () => {
+    document.getElementById("default").classList.add('hidden');
+    document.getElementById("thank-you").classList.remove('hidden');
+    document.getElementById("rating-chosen").innerText = chosenValue;
+})
