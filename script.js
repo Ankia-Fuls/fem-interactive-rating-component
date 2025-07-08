9ab06b0577945919383746769be57fe1ee0a3b4e
Ankia-Fuls <ankiafuls01@gmail.com>
2025-07-08 15:14:30 +0200

    Accessibility updated

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ let chosenValue = 0;
 const selectButton = (id) => {
     chosenValue = id.slice(-1);
     document.getElementById(`${id}`).focus();
+
+    for (let i = 1; i < 6; i++) {
+        if (i != chosenValue) {
+            document.getElementById(`button-${i}`).ariaPressed = "false";
+        }
+        else {
+            document.getElementById(`${id}`).ariaPressed = "true";
+        }
+    }
 }
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
     if (chosenValue != 0) {
         document.getElementById("default").classList.add('hidden');
         document.getElementById("thank-you").classList.remove('hidden');
